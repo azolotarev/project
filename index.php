624388afbc5add9eeaae467f72dd532f7d488ed0
Alleray <al.m.zolotarev@gmail.com>
2013-09-19 16:40:39 +0300

    1

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-hello world
+[16:08:12] Руслан: <?php
+/* Подключение к базе данных ODBC, используя вызов драйвера */
+$dsn = 'mysql:dbname=testdb;host=mysql.aksyanov.myjino.ru;port=3306';
+$user = '045527113_test';
+$password = 'qwe123';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Подключение не удалось: ' . $e->getMessage();
+}
+
+?>
